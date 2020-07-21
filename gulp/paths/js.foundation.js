33d6061f04ea1e18aa69921ac3d355578d1c4067
pichugin-ms <pichugin-ms@galagodigital.ru>
2020-07-22 01:44:10 +0500

    init

---
+++ gulp/paths/js.foundation.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = [
+  // './source/libs/jquery/jquery-3.3.1.min.js',
+  // './source/libs/slick/slick.min.js',
+  // './source/libs/swiper/swiper.min.js',
+  // './source/libs/mask/jquery.mask.min.js',
+  // './source/libs/magnific-popup/jquery.magnific-popup.min.js',
+  './source/libs/svg4everybody/svg4everybody.js'
+];
